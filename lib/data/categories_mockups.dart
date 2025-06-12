9d6028a117abb2cf6ced597c947c82a05602c016
Gabriel Torres <gabrielst328@gmail.com>
2025-06-12 18:20:47 -0300

    feat: created category model and mockups, categories screen and category grid item

---
+++ lib/data/categories_mockups.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+List<Category> registeredCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Brazilian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Spanish',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Japanese',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Thai',
+    color: Colors.purple,
+  ),
+];
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text('seu cu'),
+          Text('seu cu'),
+          Text('seu cu'),
+          Text('seu cu'),
+          Text('seu cu'),
+          Text('seu cu'),
+          Text('seu cu'),
+        ],
+      ),
+    );
+  }
+}
